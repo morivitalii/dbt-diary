fa2271fbccfae53f6b101d7040af5962f37bf674
Mori Vitalii <6964435+morivitalii@users.noreply.github.com>
2021-09-02 20:14:15 +0300

    Rails configuration (#2)

--- config/application.rb
+++ config/application.rb
@@ -3,11 +3,11 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
@@ -19,20 +19,16 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DbtDiary
+module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.schema_format = :sql
+    config.i18n.available_locales = [:ru]
+    config.i18n.default_locale = :ru
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.action_view.field_error_proc = Proc.new do |html_tag, _instance|
+      html_tag.html_safe
+    end
   end
 end
